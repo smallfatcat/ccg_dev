96bca12ca970479afe587e975083d883b6ccedb3
smallfatcat <david.imrie@gmail.com>
2015-01-13 15:22:45 +0000

    Basic Start Point

--- ccg_dev/app.js
+++ ccg_dev/app.js
@@ -1,27 +1,63 @@
-﻿var Greeter = (function () {
-    function Greeter(element) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var Entity = (function () {
+    function Entity(properties) {
+        this.id = properties.id;
+        this.xPos = properties.xPos;
+        this.yPos = properties.yPos;
     }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () {
-            return _this.span.innerHTML = new Date().toUTCString();
-        }, 500);
+    Entity.prototype.show = function () {
     };
 
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
+    Entity.prototype.hide = function () {
     };
-    return Greeter;
+
+    Entity.prototype.move = function (x, y) {
+        this.xPos += x;
+        this.yPos += y;
+    };
+
+    Entity.prototype.moveTo = function (x, y) {
+        this.xPos = x;
+        this.yPos = y;
+    };
+    return Entity;
 })();
 
+var Prop = (function (_super) {
+    __extends(Prop, _super);
+    function Prop(properties) {
+        _super.call(this, properties);
+        this.iconID = properties.iconID;
+    }
+    return Prop;
+})(Entity);
+
+var Player = (function (_super) {
+    __extends(Player, _super);
+    function Player(properties) {
+        _super.call(this, properties);
+        this.name = properties.name;
+        this.iconID = properties.iconID;
+    }
+    return Player;
+})(Entity);
+
+var g_player1 = new Player({ id: 2, xPos: 10, yPos: 20, iconID: 15, name: 'David' });
+
 window.onload = function () {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    renderPlayer(g_player1);
 };
+
+function renderPlayer(player) {
+    var playerDiv = '';
+    playerDiv += '<div id="playerDiv" class="absolute" style="left: ' + player.xPos + 'px; top: ' + player.yPos + 'px;">' + player.name + '</div>';
+    $('#content').empty();
+    $('#content').append(playerDiv);
+    //$('#playerDiv').animate({ 'left': player.xPos + 'px', 'top': player.yPos + 'px' });
+}
 //# sourceMappingURL=app.js.map
--- ccg_dev/app.ts
+++ ccg_dev/app.ts
@@ -1,28 +1,77 @@
-﻿class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+﻿class Entity {
+  id: number;
+  xPos: number;
+  yPos: number;
+  constructor(properties: EntProps) {
+    this.id = properties.id;
+    this.xPos = properties.xPos;
+    this.yPos = properties.yPos;
+  }
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
+  show() {
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
+  }
 
-    stop() {
-        clearTimeout(this.timerToken);
-    }
+  hide() {
 
+  }
+
+  move(x: number, y: number) {
+    this.xPos += x;
+    this.yPos += y;
+  }
+
+  moveTo(x: number, y: number) {
+    this.xPos = x;
+    this.yPos = y;
+  }
 }
 
+class Prop extends Entity {
+  iconID: number;
+  constructor(properties: PropProps) {
+    super(properties);
+    this.iconID = properties.iconID;
+  }
+}
+
+class Player extends Entity {
+  iconID: number;
+  name: string;
+  constructor(properties: PlayerProps) {
+    super(properties);
+    this.name = properties.name;
+    this.iconID = properties.iconID;
+  }
+}
+
+interface EntProps {
+  id: number;
+  xPos: number;
+  yPos: number;
+}
+
+interface PropProps extends EntProps {
+  iconID: number;
+}
+
+interface PlayerProps extends EntProps {
+  iconID: number;
+  name: string;
+}
+
+
+var g_player1 = new Player({ id: 2, xPos: 10, yPos: 20, iconID: 15, name: 'David' });
+
 window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+  renderPlayer(g_player1);
 };
+
+function renderPlayer(player: Player) {
+  var playerDiv: string = '';
+  playerDiv += '<div id="playerDiv" class="absolute" style="left: ' + player.xPos + 'px; top: ' + player.yPos + 'px;">' + player.name + '</div>';
+  $('#content').empty();
+  $('#content').append(playerDiv);
+  //$('#playerDiv').animate({ 'left': player.xPos + 'px', 'top': player.yPos + 'px' });
+
+}
