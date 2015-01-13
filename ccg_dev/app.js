7bda78044876b784423a3cbdaefa62eb40dee4c1
smallfatcat <david.imrie@gmail.com>
2015-01-13 17:21:31 +0000

    half done physics

--- ccg_dev/app.js
+++ ccg_dev/app.js
@@ -4,11 +4,18 @@
     __.prototype = b.prototype;
     d.prototype = new __();
 };
+var PHYSICS_TICK = 33;
+var PHYSICS_GRAVITY = 9.8;
+
 var Entity = (function () {
     function Entity(properties) {
         this.id = properties.id;
         this.xPos = properties.xPos;
         this.yPos = properties.yPos;
+        this.xVel = 0;
+        this.yVel = 0;
+        this.xAcc = 0;
+        this.yAcc = PHYSICS_GRAVITY;
     }
     Entity.prototype.show = function () {
     };
@@ -47,16 +54,56 @@ var Player = (function (_super) {
     return Player;
 })(Entity);
 
-var g_player1 = new Player({ id: 2, xPos: 10, yPos: 20, iconID: 15, name: 'David' });
+var g_player1 = new Player({ id: 1, xPos: 10, yPos: 20, iconID: 1, name: 'David' });
+var g_player2 = new Player({ id: 2, xPos: 10, yPos: 50, iconID: 2, name: 'Gary' });
 
 window.onload = function () {
-    renderPlayer(g_player1);
+    render();
+    setTimeout(physics, PHYSICS_TICK);
 };
+
+function render() {
+    $('#content').empty();
+    renderPlayer(g_player1);
+    renderPlayer(g_player2);
+}
+
+function physics() {
+    physicsPlayer(g_player1);
+    physicsPlayer(g_player2);
+    setTimeout(physics, PHYSICS_TICK);
+    render();
+}
+
+function physicsPlayer(player) {
+    // v^2 = u^2 + 2as
+    // s = u*t + 0.5 * a * t^2
+    // v = u + a*t
+    var t = PHYSICS_TICK / 1000;
+
+    var uy = player.yVel;
+    var vy = uy + (player.yAcc * t);
+    var starty = player.yPos;
+    var endy = starty + ((uy * t) + (0.5 * t * t * player.yAcc));
+
+    if (endy > 500) {
+        var s = 500 - starty;
+        vy = Math.sqrt(uy * uy * (2 * player.yAcc * s));
+        var collisionTime = (vy - uy) / player.yAcc;
+        endy = 500;
+        vy *= -0.9;
+    }
+
+    player.xPos += player.xVel;
+    player.yPos = endy;
+
+    player.xVel += player.xAcc;
+    player.yVel = vy;
+}
 
 function renderPlayer(player) {
     var playerDiv = '';
-    playerDiv += '<div id="playerDiv" class="absolute" style="left: ' + player.xPos + 'px; top: ' + player.yPos + 'px;">' + player.name + '</div>';
-    $('#content').empty();
+    playerDiv += '<div id="playerDiv' + player.id + '" class="absolute" style="left: ' + player.xPos + 'px; top: ' + player.yPos + 'px;">' + player.name + '</div>';
     $('#content').append(playerDiv);
     //$('#playerDiv').animate({ 'left': player.xPos + 'px', 'top': player.yPos + 'px' });
 }
--- ccg_dev/app.ts
+++ ccg_dev/app.ts
@@ -1,11 +1,25 @@
-﻿class Entity {
+﻿var PHYSICS_TICK: number = 33;
+var PHYSICS_GRAVITY: number = 9.8;
+
+class Entity {
   id: number;
   xPos: number;
   yPos: number;
+
+  xVel: number;
+  yVel: number;
+
+  xAcc: number;
+  yAcc: number;
+
   constructor(properties: EntProps) {
     this.id = properties.id;
     this.xPos = properties.xPos;
     this.yPos = properties.yPos;
+    this.xVel = 0;
+    this.yVel = 0;
+    this.xAcc = 0;
+    this.yAcc = PHYSICS_GRAVITY;
   }
 
   show() {
@@ -61,17 +75,57 @@ interface PlayerProps extends EntProps {
 }
 
 
-var g_player1 = new Player({ id: 2, xPos: 10, yPos: 20, iconID: 15, name: 'David' });
+var g_player1 = new Player({ id: 1, xPos: 10, yPos: 20, iconID: 1, name: 'David' });
+var g_player2 = new Player({ id: 2, xPos: 10, yPos: 50, iconID: 2, name: 'Gary' });
 
 window.onload = () => {
-  renderPlayer(g_player1);
+  render();
+  setTimeout(physics, PHYSICS_TICK);
 };
+
+function render() {
+  $('#content').empty();
+  renderPlayer(g_player1);
+  renderPlayer(g_player2);
+}
+
+function physics() {
+  physicsPlayer(g_player1);
+  physicsPlayer(g_player2);
+  setTimeout(physics, PHYSICS_TICK);
+  render();
+}
+
+function physicsPlayer(player: Player) {
+  // v^2 = u^2 + 2as
+  // s = u*t + 0.5 * a * t^2
+  // v = u + a*t
+  var t: number = PHYSICS_TICK / 1000;
+
+  var uy: number = player.yVel;
+  var vy: number = uy + (player.yAcc * t);
+  var starty: number = player.yPos;
+  var endy: number = starty + ((uy * t) + (0.5 * t * t * player.yAcc ) );
+
+  if (endy > 500) {
+    var s: number = 500 - starty;
+    vy = Math.sqrt(uy * uy * (2 * player.yAcc * s));
+    var collisionTime: number = (vy - uy) / player.yAcc;
+    endy = 500;
+    vy *= -0.9;
+  }
+
+  player.xPos += player.xVel;
+  player.yPos = endy;
+
+  player.xVel += player.xAcc;
+  player.yVel = vy;
+
+}
 
 function renderPlayer(player: Player) {
   var playerDiv: string = '';
-  playerDiv += '<div id="playerDiv" class="absolute" style="left: ' + player.xPos + 'px; top: ' + player.yPos + 'px;">' + player.name + '</div>';
-  $('#content').empty();
+  playerDiv += '<div id="playerDiv' + player.id + '" class="absolute" style="left: ' + player.xPos + 'px; top: ' + player.yPos + 'px;">' + player.name + '</div>';
   $('#content').append(playerDiv);
   //$('#playerDiv').animate({ 'left': player.xPos + 'px', 'top': player.yPos + 'px' });
-
 }
